375d62ae5c09b3864d939c13a4db8936e6f300c2
Marcus Dillavou <line72@line72.net>
2018-02-04 08:09:13 -0600

    Default to Birmingham

--- src/Configuration.js
+++ src/Configuration.js
@@ -14,16 +14,16 @@
 
 class Configuration {
     constructor() {
-        // BJCTA
-        //this.base_url = 'https://realtimebjcta.availtec.com/InfoPoint';
-        //this.center = [33.5084801, -86.8006611];
+        // Birmingham, AL (BJCTA)
+        this.base_url = 'https://realtimebjcta.availtec.com/InfoPoint';
+        this.center = [33.5084801, -86.8006611];
 
         // Arkon, OH
         //this.base_url = 'https://realtimemetro.availtec.com/InfoPoint';
 
         // Grand Rapids, MI
-        this.base_url = 'http://connect.ridetherapid.org/InfoPoint';
-        this.center = [42.956337, -85.7301293];
+        //this.base_url = 'http://connect.ridetherapid.org/InfoPoint';
+        //this.center = [42.956337, -85.7301293];
     }
 }
 
