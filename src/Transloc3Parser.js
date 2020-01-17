ace49847fe996da76b0954a496492efdb7a69397
Marcus Dillavou <line72@line72.net>
2020-01-17 12:06:09 -0600

    Started work on Transloc3Parser. Can currently get the routes and segments

---
+++ src/Transloc3Parser.js
@@ -0,0 +1,189 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2018)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+import axios from 'axios';
+import polyUtil from 'polyline-encoded';
+
+import RouteType from './RouteType';
+import VehicleType from './VehicleType';
+import StopType from './StopType';
+import ArrivalType from './ArrivalType';
+
+class Transloc3Parser {
+    constructor(agency_id, url) {
+        console.log('Transloc3');
+        this.url = url || 'https://feed.transloc.com/3/';
+        this.agency_id = agency_id;
+
+        this.requestor = axios.create({
+            baseURL: this.url,
+        });
+    }
+
+    getRoutes() {
+        // first get the segments so we can build our route paths
+        let url = `/segments.json?agencies=${this.agency_id}`;
+        return this.requestor.get(url).then((response) => {
+            let segments = response.data;
+
+            let url = `/routes.json?agencies=${this.agency_id}`;
+
+            return this.requestor.get(url).then((response) => {
+                let route_data = response.data.routes;
+
+                // parse it
+                let routes = route_data.reduce((acc, route) => {
+                    console.log('reducing', route);
+                    // build out the segments
+                    const route_segment = segments.routes.find(s => s.id === route.id);
+
+                    let polyline = route_segment.segments.map((segment) => {
+                        const s = segments.segments.find(s => s.id === Math.abs(segment));
+                        let lat_lng = polyUtil.decode(s.points);
+
+                        // !mwd - if segment is negative, then it is reversed
+                        if (segment < 0) {
+                            return lat_lng.reverse();
+                        } else {
+                            return lat_lng;
+                        }
+                    });
+
+                    console.log('new RouteType', route.id);
+                    acc[route.id] = new RouteType({
+                        id: route.id,
+                        number: route.short_name,
+                        name: route.long_name,
+                        color: route.color,
+                        polyline: polyline
+                    });
+
+                    return acc;
+                }, {});
+
+                return routes;
+            });
+        });
+    }
+
+    /**
+     * Get the stops for a specific route.
+     *
+     * @param route -> (RouteType) : The route to get the stops for
+     * @return Promise -> [StopType] : Returns a list of StopTypes
+     */
+    getStopsFor(route) {
+        return new Promise((resolve, reject) => {
+            resolve([]);
+        });
+
+        // const url = '/stops.json';
+        // return this.requestor.get(url, {params: {agencies: this.agency_id}}).then((response) => {
+        //     // Step 1: Filter stops to only this route
+        //     // Step 2: Convert to StopType
+        //     return response.data.data.filter((stop) => {
+        //         return stop.routes.includes(route.id);
+        //     }).map((stop) => {
+        //         return new StopType({
+        //             id: stop.stop_id,
+        //             name: stop.name,
+        //             position: [stop.location.lat, stop.location.lng]
+        //         });
+        //     });
+        // });
+    }
+
+    /**
+     * Get the arrivals for a stop
+     *
+     * @param stopId -> String : The id of the stop
+     * @param routes -> map(RouteType) : The dictionary of routes
+     * @return Promise -> [ArrivalType] : in sorted order
+     */
+    getArrivalsFor(stopId, routes) {
+        return new Promise((resolve, reject) => {
+            resolve([]);
+        });
+        // const url = '/arrival-estimates.json'
+        // return this.requestor.get(url, {params: {agencies: this.agency_id, stops: stopId}})
+        //     .then((response) => {
+        //         // !mwd - there should ONLY be one item in the list
+        //         //  because we have only requested a single stopId.
+        //         // We could potentially do some filtering, but maybe later...
+        //         const arrivals = response.data.data.flatMap((stop) => {
+        //             return stop.arrivals.map((arrival) => {
+        //                 return new ArrivalType({
+        //                     route: routes[arrival.route_id],
+        //                     direction: '',
+        //                     arrival: arrival.arrival_at
+        //                 });
+        //             });
+        //         });
+
+        //         // sort based on arrival time
+        //         return arrivals.sort((a, b) => {
+        //             if (a.arrival.isBefore(b.arrival)) {
+        //                 return -1;
+        //             } else if (a.arrival.isSame(b.arrival)) {
+        //                 return 0;
+        //             } else {
+        //                 return 1;
+        //             }
+        //         });
+        //     });
+    }
+
+    getVehicles(bounds, visible_routes) {
+        return new Promise((resolve, reject) => {
+            resolve({});
+        });
+        // let url = `/vehicles.json?agencies=${this.agency_id}`;
+
+        // if (bounds != null) {
+        //     // filter vehicles not within the bounds
+        //     // !mwd - we expand the search region a little bit
+        //     //  so that vehicles don't jump in and out of the screen
+        //     let epsilon = 0.008;
+        //     let bottomLeft = [bounds["_southWest"]["lat"] - epsilon, bounds["_southWest"]["lng"] - epsilon];
+        //     let topRight = [bounds["_northEast"]["lat"] + epsilon, bounds["_northEast"]["lng"] + epsilon];
+
+        //     url=`${url}&geo_area=${bottomLeft[0]},${bottomLeft[1]}|${topRight[0]},${topRight[1]}`;
+        // }
+
+        // return this.requestor.get(url).then((response) => {
+        //     let vehicle_data = response.data.data[this.agency_id] || []
+
+        //     return vehicle_data.reduce((acc, vehicle) => {
+        //         let v = new VehicleType({
+        //             id: vehicle.vehicle_id,
+        //             position: [vehicle.location.lat, vehicle.location.lng],
+        //             heading: vehicle.heading,
+        //             destination: '',
+        //             on_board: vehicle.passenger_load || '',
+        //             route_id: vehicle.route_id
+        //         });
+
+        //         if (v.route_id in acc) {
+        //             acc[v.route_id].push(v);
+        //         } else {
+        //             acc[v.route_id] = [v];
+        //         }
+
+        //         return acc;
+        //     }, {});
+        // });
+    }
+}
+
+export default Transloc3Parser;
