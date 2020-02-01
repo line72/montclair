f0c9e7855f833040fda42d12cdb616fc633b72e4
Marcus Dillavou <line72@line72.net>
2020-02-01 09:38:15 -0600

    Stubbed out the GTFSRTParser

---
+++ src/GTFSRTParser.js
@@ -0,0 +1,83 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2020)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+class GTFSRTParser {
+    constructor() {
+
+    }
+
+    /**
+     * Get the routes.
+     *
+     * @return Promise -> map(Id,RouteType) : Returns a map of RouteTypes by Id
+     */
+    getRoutes() {
+        return new Promise((success, failure) => {
+            success([]);
+        });
+    }
+
+    /**
+     * Get the stops for a specific route.
+     *
+     * @param route -> (RouteType) : The route to get the stops for
+     * @return Promise -> [StopType] : Returns a list of StopTypes
+     */
+    getStopsFor(route) {
+        return new Promise((success, failure) => {
+            success([]);
+        });
+    }
+
+    /**
+     * Get the arrivals for a stop
+     *
+     * @param stopId -> String : The id of the stop
+     * @param routes -> map(RouteType) : The dictionary of routes
+     * @return Promise -> [ArrivalType] : in sorted order
+     */
+    getArrivalsFor(stopId, routes) {
+        return new Promise((success, failure) => {
+            success([]);
+        });
+    }
+
+    /**
+     * Get the vehicles for an area or a list of routes
+     *
+     * @param bounds -> ([LatLng]) : The leaflef bounds of the map
+     * @param visible_routes -> ([RouteType]) : The list of routes
+     * @return Promise -> map(RouteId,VehicleType) : Returns a map of VehicleType by RouteId
+     */
+    getVehicles(bounds, visible_routes) {
+        return new Promise((success, failure) => {
+            success([]);
+        });
+    }
+
+    /**
+     * Get a specific vehicle on a specific route
+     *
+     * @param route -> RouteType : The route
+     * @param vehicleId -> String : The id of the vehicle, this matches the Trip's BlockFareboxId
+     * @return Promise -> VehicleType | nil : The vehicle if found
+     */
+    getVehicle(route, vehicleId) {
+        return new Promise((success, failure) => {
+            success([]);
+        });
+    }
+}
+
+export default GTFSRTParser;
